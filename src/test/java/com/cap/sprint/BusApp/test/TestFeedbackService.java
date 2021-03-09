6840d889997271451d79184b5421e21817e5ea91
Sajin-storm <Aistator@gmail.com>
2021-03-09 09:10:57 +0530

    TestFeedbackService

---
+++ src/test/java/com/cap/sprint/BusApp/test/TestFeedbackService.java
@@ -0,0 +1,49 @@
+package com.cap.sprint.BusApp.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cap.sprint.BusApp.entities.BusOperator;
+import com.cap.sprint.BusApp.entities.Feedback;
+import com.cap.sprint.BusApp.services.FeedbackService;
+
+@SpringBootTest
+class TestFeedbackService {
+	
+	@Autowired
+	FeedbackService feedbackService;
+	
+	@Test
+	void testAddFeedback() {
+		Feedback feedback = new Feedback();
+		feedback.setRouteName("A to B");
+		feedback.setUsername("Arijith");
+		feedback.setRating(5);
+		feedbackService.addFeedback(feedback);
+	}
+
+	@Test
+	void testViewAllFeedbacksStringBusOperator() {
+		BusOperator busOperator = new BusOperator();
+		busOperator.setBusOperatorUsername("Ravi");
+		busOperator.setPassword("Ravi123");
+		feedbackService.viewAllFeedbacks("A to B", busOperator);
+	}
+
+	@Test
+	void testViewAllFeedbacksBusOperator() {
+		BusOperator busOperator = new BusOperator();
+		busOperator.setBusOperatorUsername("Ravi");
+		busOperator.setPassword("Ravi123");
+		feedbackService.viewAllFeedbacks(busOperator);
+	}
+
+	@Test
+	void testGetAllFeedbacks() {
+		feedbackService.getAllFeedbacks();
+	}
+
+}
